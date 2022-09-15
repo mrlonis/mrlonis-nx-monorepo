15bfde44b802b1d36e4d41181d4b90cb14de202c
Matthew L <mattlonis@gmail.com>
2022-09-14 21:22:34 -0400

    Adding shared angular library

--- apps/ui-mythic-heroes/src/app/app.component.spec.ts
+++ apps/ui-mythic-heroes/src/app/app.component.spec.ts
@@ -25,8 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome ui-mythic-heroes'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome ui-mythic-heroes');
   });
 });
---
+++ libs/ngx-mrlonis-shared/README.md
@@ -0,0 +1,7 @@
+# ngx-mrlonis-shared
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ngx-mrlonis-shared` to execute the unit tests.
