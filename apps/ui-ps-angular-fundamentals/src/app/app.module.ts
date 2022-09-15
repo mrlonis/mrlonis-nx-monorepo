3e4775340aeb5eea9bad6bf8562fe28dd7034317
Matthew L <mattlonis@gmail.com>
2022-09-14 21:43:18 -0400

    Adding all other apps

---
+++ apps/ui-ps-angular-fundamentals/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }), BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
