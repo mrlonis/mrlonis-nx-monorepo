7a612c442f68a75415a913e6363c28936630f3a1
Matthew L <mattlonis@gmail.com>
2022-12-08 13:35:43 -0500

    Convertted ngx-test-application to karma

--- apps/ngx-test-application/src/app/features/function-testing/function-testing.component.spec.ts
+++ apps/ngx-test-application/src/app/features/function-testing/function-testing.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../../shared';
 import { FunctionTestingComponent } from './function-testing.component';
 
 describe('FunctionTestingComponent', () => {
@@ -8,6 +10,7 @@ describe('FunctionTestingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FunctionTestingComponent],
+      imports: [NoopAnimationsModule, SharedModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FunctionTestingComponent);
